999074f4da9a569b5f5c55cce7b83c6aa4875480
noappert <noappertfr@gmail.com>
2025-08-12 18:21:59 +0200

    x

--- capadi.sh
+++ capadi.sh
@@ -1,8 +1,4 @@
-if[ $1 == "bg"]
-then
-    echo "CAPADI: Backgrounding the background..."
-    curl -L https://raw.githubusercontent.com/capadi/capadi/main/img/bg.png -o ./bg.png
-    gsettings set org.gnome.desktop.background picture-uri-dark 'bg.png'
-    gsettings set org.gnome.desktop.background picture-uri 'bg.png'
-else
-    echo "skill issue"
+echo "CAPADI: Backgrounding the background..."
+curl -L https://raw.githubusercontent.com/capadi/capadi/main/img/bg.png -o ./bg.png
+gsettings set org.gnome.desktop.background picture-uri-dark 'bg.png'
+gsettings set org.gnome.desktop.background picture-uri 'bg.png'
