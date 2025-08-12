cd88e3fdc1397fd5444c8ca0dcedf712422540f0
noappert <noappertfr@gmail.com>
2025-08-12 18:26:57 +0200

    gh

--- capadi.sh
+++ capadi.sh
@@ -1,4 +1,4 @@
 echo "CAPADI: Backgrounding the background..."
 curl -L https://raw.githubusercontent.com/capadi/capadi/main/img/bg.png -o ./bg.png
-gsettings set org.gnome.desktop.background picture-uri-dark 'bg.png'
-gsettings set org.gnome.desktop.background picture-uri 'bg.png'
+gsettings set org.gnome.desktop.background picture-uri-dark './bg.png'
+gsettings set org.gnome.desktop.background picture-uri './bg.png'
