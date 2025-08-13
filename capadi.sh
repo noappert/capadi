6b52e7ec128a78a8778ae275c7b230f77075ab8a
noappert <noappertfr@gmail.com>
2025-08-13 07:50:50 +0200

    ss

--- capadi.sh
+++ capadi.sh
@@ -7,5 +7,5 @@ else
     RANDOMBG=$((RANDOM%1))
 fi
 curl -L https://raw.githubusercontent.com/noappert/capadi/refs/heads/main/img/${RANDOMBG}.jpg -o ./bg.png
-gsettings set org.gnome.desktop.background picture-uri-dark './${RANDOMBG}.png'
-gsettings set org.gnome.desktop.background picture-uri './${RANDOMBG}.png'
+gsettings set org.gnome.desktop.background picture-uri-dark './bg.png'
+gsettings set org.gnome.desktop.background picture-uri './bg.png'
