0ae493589f80d2d692373d73ed1f4c0ee9391fe5
nappert <nappert@student.42angouleme.fr>
2025-08-14 09:52:20 +0200

    e

--- capadi.sh
+++ capadi.sh
@@ -2,7 +2,7 @@ clear
 echo "CAPADI: Backgrounding the background..."
 
 echo "CAPADI: choix aléatoire."
-RANDOMBG=$(( (RANDOM % 1) ))
+RANDOMBG=$(( (RANDOM % 2) ))
 
 curl -L "https://raw.githubusercontent.com/noappert/capadi/refs/heads/main/img/${RANDOMBG}.jpg" -o ./bg.jpg
 gsettings set org.gnome.desktop.background picture-uri-dark './bg.jpg'
